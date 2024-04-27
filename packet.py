e5d2ff6e1fa69e5fc0a9ecc02fd8488ed5b6240a
mjkadzi <161668993+mjkadzi@users.noreply.github.com>
2024-04-26 21:30:12 -0500

    Update packet.py
    might be an idiot for trying to recalc checksums in this case

--- packet.py
+++ packet.py
@@ -1,47 +1,42 @@
 from scapy.all import IP, UDP, NTP, send, sniff
 from scapy.layers.ntp import NTP
-import time
+import time, datetime
+
 
 server_ip = "10.10.10.12"
 dest_ip = "10.10.10.10"
 ref_timestamp = time.time() + 259200
 
-def send_ntp(server, packetsrc, ref_timestamp):
-    ntp_packet = NTP()
+def send_ntp(server, packet, ref_timestamp):
+    ntp_packet = packet
+    print("incoming packet")
+    #ntp_packet.show()
+    print()
 
-    ntp_packet.leap = 00
-    ntp_packet.ref = ref_timestamp
-    ntp_packet.version = 3
-    ntp_packet.ref_id = b"\x00\x00\x00\x00"
-    ntp_packet.delay = 1
-    ntp_packet.dispersion = 16
-    ntp_packet.precision = 1
-    ntp_packet.poll = 4
-    ntp_packet.mode = 4
-    ntp_packet.id = server
-    ntp_packet.stratum = 1
-    ntp_packet.orig = time.time() + 259201
-    ntp_packet.recv = time.time() + 259201
-    ntp_packet.sent =time.time() + 259201
+    ntp_packet[NTP].ref = ref_timestamp
+    ntp_packet[NTP].orig = time.time() + 259201
+    ntp_packet[NTP].recv = time.time() + 259201
+    ntp_packet[NTP].sent = time.time() + 259201
 
-    ip_packet = IP(src=server, dst=dest_ip)
-    udp_packet = UDP(dport=123, sport=123)
+    #ip_packet = IP(src=server, dst=dest_ip)
+    #udp_packet = UDP(dport=123, sport=123)
+    #print("Packet before sending")
+    #ntp_packet.show()
+    #ip_packet.show()
+    #udp_packet.show()
+    #if ntp_packet.haslayer(UDP):
+    #    print("new checksum: " + str(ntp_packet[UDP].chksum))
+    #else:
+    #    print("Packet does not contain UDP layer")
+    ntp_packet = ntp_packet.build()
+    send(ntp_packet, verbose=True)
 
-    print("Packet before sending")
-    ntp_packet.show()
-    ip_packet.show()
-    udp_packet.show()
-
-    send(ip_packet/udp_packet/ntp_packet, verbose=True)
 
 def ntp_request(packet):
     ntp_layer = packet.getlayer(NTP)
-    if ntp_layer and ntp_layer.mode == 3:
-        print("Here is the ref_id")
-        print(ntp_layer.ref_id)
-        print("here is the ref_time")
-        print(ntp_layer.ref)
-        send_ntp(server_ip, packet[IP].src, ref_timestamp)
+    if ntp_layer and ntp_layer.mode == 4:
+        print("old checksum: " + str(packet[UDP].chksum))
+        send_ntp(server_ip, packet, ref_timestamp)
 
 if __name__ == "__main__":
     sniff(filter="udp and port 123", prn=ntp_request)
